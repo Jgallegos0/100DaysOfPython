0a46b9ff91c455119ec7f4e96f0519368b45382f
Jeannette Mayo <65695953+Jgallegos0@users.noreply.github.com>
2021-01-26 15:21:03 -0800

    Add files via upload

---
+++ EscapingTheMaze.py
@@ -0,0 +1,20 @@
+#Reborg.com Maze Challenge:
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    else:
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
+           
+                
+  
+    
